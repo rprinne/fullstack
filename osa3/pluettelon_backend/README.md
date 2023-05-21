db157a5cd7c7a676cf79faf08937207e314240f5
rprinne <rp.rinne@hotmail.com>
2023-05-21 23:53:42 +0300

    connected backendto mongodb atlas

---
+++ osa3/pluettelon_backend/README.md
@@ -0,0 +1,1 @@
+https://puhelinluetto.onrender.com/
--- osa3/pluettelon_backend/index.js
+++ osa3/pluettelon_backend/index.js
@@ -1,89 +1,101 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
 const app = express()
 
-const randomBetween = (a,b) => {
-  return Math.floor(Math.random() * (b-a) + a)
-}
-
-let persons = [
-        {
-          "name": "Arto Hellas",
-          "number": "040-123456",
-          "id": 1
-        },
-        {
-          "name": "Ada Lovelace",
-          "number": "39-44-5323523",
-          "id": 2
-        },
-        {
-          "name": "Dan Abramov",
-          "number": "12-43-234345",
-          "id": 3
-        },
-        {
-          "name": "Mary Poppendick",
-          "number": "39-23-6423122",
-          "id": 4
-        }
-]
-
+// Morgan /////////////////////////////////
 morgan.token('request_body', (req, res) => {
-  if (req.method == 'POST') { return JSON.stringify(req.body) }
+  if (req.method === 'POST' || req.method === 'PUT') { return JSON.stringify(req.body) }
 })
 app.use(morgan('tiny'))
+////////////////////////////////////////////
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
 
-app.use(express.json())
+const errorHandler = (error, req, res, next) => {
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
 app.use(express.static('build'))
+app.use(express.json())
 
 app.get('/info', (req, res) => {
-  const time = Date(Date.now())
-  res.send(`<p>Phonebook has info for ${persons.length} people.</p><p>${time.toString()}</p>`)
+  Person.find({}).then(people => {
+    const time = Date(Date.now())
+    const count = people.length
+    res.send(`<p>Phonebook has info for ${count} people.</p><p>${time.toString()}</p>`)
+  })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(p => p.id === id)
-    if (person) {
-      res.json(person)
-    } else {
-      res.status(404).end()
-    }
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end()
+      }
   })
+  .catch(error => next(error))
+})
 
 app.post('/api/persons', morgan(':method :url :status - :response-time ms :request_body'), (req, res) => {
-  const person = {...req.body, id: randomBetween(100, 1000000)}
-  if (persons.filter(p => p.name === person.name).length) {
-    res.status(400).send({error: 'name must be unique'})
-  } else if (person.name && person.number) {
-    persons = [...persons, person]
-    res.status(200).json(person)
-  } else {
-    res.status(400).send({error: 'name or number missing' })
+  const body = req.body
+  if (body.name === undefined) {
+    return res.status(400).json({error: 'content missing'})
   }
+  const person = new Person({
+    name: body.name,
+    number: body.number || 0
+  })
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
 
-app.put('/api/persons/:id', (req, res) => {
-  const number = req.body.number
-  const id = Number(req.params.id)
-  console.log(number, id)
-  persons = persons.map(p => p.id === id ? {...p, number: number} : p)
-  res.status(200).end()
+app.put('/api/persons/:id', morgan(':method :url :status - :response-time ms :request_body'), (req, res, next) => {
+  const body = req.body
+  if (body.name === undefined) {
+    return res.status(400).json({error: 'name missing'})
+  }
+  if (body.number === undefined) {
+    return res.status(400).json({error: 'number missing'})
+  }
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+  Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    .then(person => {
+      if (person) {
+        res.json(person)
+      } else {
+        res.status(404).end
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  persons = persons.filter(p => p.id !== id)
-  console.log(persons)
-  res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+app.use(errorHandler)
