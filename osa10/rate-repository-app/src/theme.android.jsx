51b71d0f7e3d153ab0e5631e4d024adfaf4c8fb2
rprinne <rp.rinne@hotmail.com>
2025-11-23 00:14:35 +0200

    10.10 tehty

---
+++ osa10/rate-repository-app/src/theme.android.jsx
@@ -0,0 +1,21 @@
+const theme = {
+  colors: {
+    textPrimary: '#ca860fff',
+    textSecondary: '#586069',
+    primary: '#0366d6',
+    textWhite: 'white',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 16,
+  },
+  fonts: {
+    main: 'Roboto',
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;
